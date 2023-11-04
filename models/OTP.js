f656820df404217933bc837a334a29ef9047c379
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-11-04 11:10:45 +0530

    Added code for sending email using nodemailer and creting pre hoke in otp model

--- models/OTP.js
+++ models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 const OTPSchema = mongoose.Schema({
     email: {
         type:String,
@@ -15,4 +16,21 @@ const OTPSchema = mongoose.Schema({
     }
 });
 
+// a function -> to send emails
+async function sendVerificationEmail(email, otp) {
+    try {
+      const mailResponse =  await mailSender(email, "Verification Email from EdTech Platfrom", otp);
+
+    } catch (error) {
+        console.log("Error occured while sending mails", error);
+        throw error;
+    }
+
+}
+
+// pre middleware
+OTPSchema.pre("save", async function(next){
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+});
 module.exports = mongoose.model("OTP", OTPSchema);
