edf50e4ecd334bda1fc39b241651558009cb71f3
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-11-08 07:01:16 +0530

    Added auth middleware that verify the toke

---
+++ middlewares/auth_middleware.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+const User = require('../models/User');
+
+//auth
+exports.auth = async(req, re, next) => {
+    try {
+      // extract token
+      const token = req.cookies.token || req.body || req.header("Authorisation").replace("Bearer ", ""); 
+      // if token missing, then return responce
+      if(!token){
+        return res.status(401).json({
+            success: false,
+            message: 'Token is missing'
+        });
+      }
+
+      //verify the token using secret key
+      try {
+        const decode = await jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decode;
+      } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: 'token is invalid'
+        });
+      }
+      next();
+
+    } catch (error) {
+       return res.status(401).json({
+        success: false,
+        message: 'Something went wrong while validating the token'
+    });
+    }
+}
