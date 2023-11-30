63e680d61ab7ddfc0dbd913035a7885195e9a4fa
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-11-30 20:07:01 +0530

    Added: course details handler inside Course_ Controller

--- controllers/Course_Controller.js
+++ controllers/Course_Controller.js
@@ -152,3 +152,49 @@ exports.getAllCourses = async (req, res) => {
 		});
 	}
 };
+
+
+//getCourseDetails
+exports.getCourseDetails = async(req, res) => {
+	try {
+		// get id
+		const {courseId} = req.body;
+		//find course details
+		const courseDetails = await Course.find({_id:courseId}).populate(
+			                                                             {
+																			path:"instructor",
+																			populate:{
+																				path:"additionalDetails"
+																			}
+			                                                             }
+																	 )
+																	 .populate("category")
+																	 .populate("ratingAndreviews")
+																	 .populate({
+																		path:"courseContent",
+																		populate:{
+																			path:"subSection"
+																		}
+																	 }).exec();
+		// validation
+		if(!courseDetails){
+			return res.status(400).json({
+				success: false,
+				message: `Could not find the course with ${courseId}`
+			});
+		}
+		// return response
+		return res.status(200).json({
+			success: true,
+			message: `CourseDetails fetched details Successfully`,
+			data: courseDetails
+		});
+
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			success: false,
+			message: "Internal Server Error"
+		});
+	}
+}
