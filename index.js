c2774a508f0252d1923edf418471f04f34e6d74d
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-12-02 18:16:53 +0530

    Added: Router

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const express = require("express");
-const app = express();
-const db = require("./config/database.js")
-require("dotenv").config();
-// Port find
-const PORT = process.env.PORT || 3000;
-// db connection
-db.connect();
 
-// active server
-app.listen(PORT, () => {
-    console.log(`App is running at ${PORT}`);
-})
